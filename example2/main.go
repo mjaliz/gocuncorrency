dd968be19ca1723beb6cda535b2626dea8045785
MohammadJavad14 <mrph14@gmail.com>
2023-06-23 23:18:21 +0330

    a more complex example

--- example2/main.go
+++ example2/main.go
@@ -5,25 +5,47 @@ import (
 	"sync"
 )
 
-var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string, m *sync.Mutex) {
-	defer wg.Done()
-
-	m.Lock()
-	msg = s
-	m.Unlock()
+type Income struct {
+	Source string
+	Amount int
 }
 
 func main() {
-	var mutex sync.Mutex
-	msg = "Hello, world!"
 
-	wg.Add(2)
-	go updateMessage("Hello, universe!", &mutex)
-	go updateMessage("Hello, cosmos!", &mutex)
+	var bankBalance int
+	var balance sync.Mutex
+
+	fmt.Printf("Initial account balance: $%d.00", bankBalance)
+	fmt.Println()
+
+	incomes := []Income{
+		{Source: "Main job", Amount: 500},
+		{Source: "Gift", Amount: 10},
+		{Source: "Part time job", Amount: 50},
+		{Source: "Investments", Amount: 20},
+	}
+
+	wg.Add(len(incomes))
+
+	for i, income := range incomes {
+
+		go func(i int, income Income) {
+			defer wg.Done()
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+				fmt.Printf("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+			}
+		}(i, income)
+	}
+
 	wg.Wait()
 
-	fmt.Println(msg)
+	fmt.Printf("Final bank balance: $%d.00", bankBalance)
+	fmt.Println()
 }
